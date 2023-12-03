2943ed1386cef090f090eb096cc240ae17889d50
Vlad-Petruk <vladpetruk.00@gmail.com>
2023-12-03 14:14:50 +0200

    added function for rendering and loading DOM

--- src/UI.js
+++ src/UI.js
@@ -1,1 +1,42 @@
 import { TodoFactory, ProjectFactory } from "./factories";
+
+const domLoader = () =>{
+    const navigation = document.getElementById('navigation');
+    const content = document.getElementById('content');
+  
+    // Sample data for navigation items and corresponding content
+    const sections = [
+      { id: 'section1', title: 'Section 1', content: 'Content for Section 1' },
+      { id: 'section2', title: 'Section 2', content: 'Content for Section 2' },
+      // Add more sections as needed
+    ];
+
+    function renderNavigation() {
+      sections.forEach(section => {
+        const item = document.createElement('div');
+        item.textContent = section.title;
+        item.addEventListener('click', () => showContent(section));
+        navigation.appendChild(item);
+      });
+    }
+  
+    function showContent(section) {
+      // Hide previous content
+      content.innerHTML = '';
+      // Display new content
+      const contentElement = document.createElement('div');
+      contentElement.textContent = section.content;
+      content.appendChild(contentElement);
+    }
+  
+    // Initial render
+    renderNavigation();
+
+    return {
+        sections,
+        renderNavigation,
+        showContent
+    }
+}
+
+export {domLoader}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import { TodoFactory, ProjectFactory } from "./factories";
+import { domLoader } from "./UI";
+
+// i need to whrite local storage functions, peghaps in different module
+// add date-fns
+// create or find the UI: 
+// expand the factories with the local storage functions(delete,add,get)
+// create onload function with all get from local storage
+// learn how to expand todo to see/edit its details
+// view all todos in each project (probably just the title and duedate… perhaps changing color for different priorities)
+
+document.addEventListener('DOMContentLoaded', function () {
+    domLoader();
+   });
+  
